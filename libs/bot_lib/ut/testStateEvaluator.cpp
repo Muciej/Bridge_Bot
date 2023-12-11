640a95a005cde43f1fdafb09607e6433463dc49f
Maciej Józefkowicz <maciej.jozefkowicz@gmail.com>
2023-12-11 01:32:29 +0100

    Adding tests for StateEvaluator (switching to laptop).

---
+++ libs/bot_lib/ut/testStateEvaluator.cpp
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+#include <bot_lib/bot_utils/GlobalGameState.hpp>
+#include <bot_lib/bot_utils/GameState.hpp>
+#include <bot_lib/state_evaluator/BaseEvaluator.hpp>
+
+using bot::GameState;
+using bot::GlobalGameState;
+
+namespace
+{
+
+GameState getExampleGameState()
+{
+
+}
+
+GlobalGameState getExampleGlobalGameState()
+{
+
+}
+
+void fill_example_card_point_table_1(int card_points[4][52])
+{
+
+}
+
+} // namespace
+
+TEST(StateEvaluatorTests, )
